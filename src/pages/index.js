5d6d55700ef8e3a39a288d8ad5b58c6219983df8
Bartosz <bartosz.sroka4@gmail.com>
2020-07-06 16:29:48 +0200

    core of mobile layout finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,42 @@
-import React from "react"
+import React, { Component } from "react"
+import { createGlobalStyle } from "styled-components"
 
-const IndexPage = () => <h1>test czy działa</h1>
+import Navigation from "../templates/Navigation"
+import HelloSection from "../templates/HelloSection"
+import AboutMeSection from "../templates/AboutMeSection"
+import MyFreeTime from "../templates/MyFreeTime"
+import Blog from "../templates/Blog"
+import Contact from "../templates/Contact"
+
+const GlobalStyle = createGlobalStyle`
+    body, h1,h2,h3,h4,h5,h6{
+        margin:0;
+        padding:0;
+        font-family: "Charter";
+        font-weight: 100;
+        font-style: italic;
+        color: #6b6b6b;
+    }
+    *, *::before, *::after{
+        box-sizing: border-box;
+    }
+`
+
+class IndexPage extends Component {
+  state = {}
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <Navigation />
+        <HelloSection />
+        <AboutMeSection />
+        <MyFreeTime />
+        <Blog />
+        <Contact />
+      </>
+    )
+  }
+}
 
 export default IndexPage
