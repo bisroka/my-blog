69632b54077639658f3fcbf1f95c550ecf365015
Bartosz <bartosz.sroka4@gmail.com>
2021-03-23 23:22:28 +0100

    Add animation

--- src/Components/ContactIcons.js
+++ src/Components/ContactIcons.js
@@ -6,7 +6,7 @@ import PropTypes from "prop-types"
 const ContactIcons = ({ contactPictures }) => {
   const contactPicture = contactPictures.map(picture => (
     <a key={picture.id} href={picture.link}>
-      <img src={picture.src} />
+      <img className="social-icon" src={picture.src} />
     </a>
   ))
   return <StyledContactIcons>{contactPicture}</StyledContactIcons>
