aece5057efe989eb6580623a0f3c21a20f03f85b
Bartosz <bartosz.sroka4@gmail.com>
2021-03-26 22:05:33 +0100

    Fix bug with non hidden mobile menu

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from "react"
-
+import { StyledPageWrapper } from "../utils/pageWrapper"
 const NotFoundPage = () => {
     return(
         <>
+        <StyledPageWrapper>
             <h1>Nie ma takiej strony</h1>
             <h2><a href="/">Wróć do strony głównej</a></h2>
+        </StyledPageWrapper>
         </>
     )
 }
